80dac39da2b734fc4e37dd1e62752ee859eda5cb
ygxdxx <aaa100007673@163.com>
2017-10-21 14:26:25 +0800

    insert sort

---
+++ algorithms/ex10_insert_sort.py
@@ -0,0 +1,22 @@
+#! python3
+
+
+def insert_sort(lst):
+    """插入排序"""
+    n = len(lst)
+    for i in range(1, n):
+        for j in range(i, 0, -1):
+            if lst[j] < lst[j - 1]:
+                lst[j], lst[j - 1] = lst[j - 1], lst[j]
+            else:
+                break
+
+
+if __name__ == '__main__':
+    data = [17, 20, 93, 54, 77, 31, 44, 54, 226]
+    # print(data)
+    # insert_sort(data)
+    # print(data)
+    s = 'abcde'
+    arr = s.split()
+    print(len(arr))
